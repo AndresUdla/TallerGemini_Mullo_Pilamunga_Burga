759c17796a063d67c36d568826f81c50479dcd29
Josue Mullo <josue.mullo@udla.edu.ec>
2025-05-07 07:51:45 -0500

    Creacion del Repositorio GeminiRepository para el manejo de la logica del programa

---
+++ TallerGemini_Mullo_Pilamunga_Burga/Repositories/GeminiRepository.cs
@@ -0,0 +1,12 @@
+﻿using TallerGemini_Mullo_Pilamunga_Burga.Interfaces;
+
+namespace TallerGemini_Mullo_Pilamunga_Burga.Repositories
+{
+    public class GeminiRepository : IChatBotService
+    {
+        public Task<string> GetChatBotResponse(string prompt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
