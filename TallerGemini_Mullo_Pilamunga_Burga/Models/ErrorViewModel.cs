6fcbb6e0cc6ee5820a0170f2cb8257ef0499a285
Josue Mullo <josue.mullo@udla.edu.ec>
2025-05-07 07:44:31 -0500

    Add project files.

---
+++ TallerGemini_Mullo_Pilamunga_Burga/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TallerGemini_Mullo_Pilamunga_Burga.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
